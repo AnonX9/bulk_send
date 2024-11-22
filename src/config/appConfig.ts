d60cfdf0dc505110d272d7093eec2505f7a7c75d
AnonX9 <avaikamarcel@gmail.com>
2024-11-22 14:47:55 +0100

    update emoji

--- src/config/appConfig.ts
+++ src/config/appConfig.ts
@@ -3,7 +3,7 @@ export const appConfig = {
 We're thrilled to remind you that CodeConnect 2 is happening tomorrow! 🚀
 📅 Date: November 23
 ⏰ Time: 9:00 AM
-📍 Location: Hotel Prince de Galle
+🚀 Location: Hotel Prince de Galle
 Get ready for an exciting day of learning, networking, and career-shaping insights. We can't wait to see you there!
 The CodeConnect Team`
 }
